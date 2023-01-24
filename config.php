0554841210959b6ae8a8d7c6cbcf6c15e324c57b
Fredrik Leonard Stenersen <fredrlst@ntnu.no>
2023-01-24 12:29:41 +0000

    Added config php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost = "192.168.132.116";
+$dbport = "26257";
+$db = "bf";
+$dbuser = "zuck";
+$dbpassw = ''; 
+$webhost = '10.212.174.5';
+$weburl = 'http://' . $webhost ;
+$frontpage_limit = "1000";
+?>
