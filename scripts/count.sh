2e379c8682c9e287af0b1c5f4d4365db670a61f1
Fredrik Leonard Stenersen <fredrlst@stud.ntnu.com>
2023-03-15 14:40:58 +0100

    count.sh created

--- scripts/count.sh
+++ scripts/count.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-// Finner antall brukere i bookface
-cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM users;"
+# Finner antall brukere i bookface
+cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM users;"  | tail -n +3 | head -n 1 | awk '{print "Antall brukere\t\t" $1}'
+
+# Finner antall poster i bookface
+cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM posts;" | tail -n +3 | head -n 1 | awk '{print "Antall poster\t\t" $1}'
+
+# Finner antall kommentarer i bookface
+cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM comments;" | tail -n +3 | head -n 1 | awk '{print "Antall kommentarer\t" $1}'
