fc7ced9cab2086246058a1675a1512ca5d948e1c
Fredrik Leonard Stenersen <fredrlst@stud.ntnu.com>
2023-03-15 14:57:56 +0100

    fixed

--- scripts/count.sh
+++ scripts/count.sh
@@ -2,10 +2,8 @@
 source ./base.sh
 
 # Finner antall brukere i bookface
-brukere="$(cockroach --insecure --host=localhost sql --execute="USE bf; \
-SELECT COUNT(*) FROM users;" | tail -n +3 | head -n 1 | \
-awk '{print "Antall brukere\\t\\t" $1}')
-info "$brukere""
+brukere=$(cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM users;" | tail -n +3 | head -n 1 | awk '{print "Antall brukere\\t\\t" $1}')
+info "$brukere"
 
 # Finner antall poster i bookface
 poster=$(cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM posts;" | tail -n +3 | head -n 1 | awk '{print "Antall poster\t\t" $1}')
