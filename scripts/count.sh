ae426fa259cf2466866b8259be02c0eab5f792f5
Fredrik Leonard Stenersen <fredrlst@stud.ntnu.com>
2023-03-15 15:00:36 +0100

    fix

--- scripts/count.sh
+++ scripts/count.sh
@@ -2,16 +2,13 @@
 source ./base.sh
 
 # Finner antall brukere i bookface
-brukere=$(cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM users;" \\
-    | tail -n +3 | head -n 1 | awk '{print "Antall brukere\\t\\t" $1}')
+brukere=$(cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM users;" | tail -n +3 | head -n 1 | awk '{print "Antall brukere\\t\\t" $1}')
 info "$brukere"
 
 # Finner antall poster i bookface
-poster=$(cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM posts;" \\
-    | tail -n +3 | head -n 1 | awk '{print "Antall poster\t\t" $1}')
+poster=$(cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM posts;" | tail -n +3 | head -n 1 | awk '{print "Antall poster\t\t" $1}')
 info "$poster"
 
 # Finner antall kommentarer i bookface
-kommentarer=$(cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM comments;" \\
-    | tail -n +3 | head -n 1 | awk '{print "Antall kommentarer\t" $1}')
+kommentarer=$(cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM comments;" | tail -n +3 | head -n 1 | awk '{print "Antall kommentarer\t" $1}')
 info "$kommentarer"
