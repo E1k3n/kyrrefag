10a2c20c3ff5b87ed0480b0bf59f7ca58ba2eb0a
Fredrik Leonard Stenersen <fredrlst@stud.ntnu.com>
2023-03-15 14:46:30 +0100

    testing

--- scripts/count.sh
+++ scripts/count.sh
@@ -3,7 +3,7 @@ source ./base.sh
 
 # Finner antall brukere i bookface
 users=$(cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM users;" | tail -n +3 | head -n 1 | awk '{print "Antall brukere\\t\\t" $1}')
-info users
+info $users
 
 # Finner antall poster i bookface
 cockroach --insecure --host=localhost sql --execute="USE bf; SELECT COUNT(*) FROM posts;" | tail -n +3 | head -n 1 | awk '{print "Antall poster\t\t" $1}'
